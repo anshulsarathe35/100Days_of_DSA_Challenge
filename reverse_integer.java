5dd4dcaca085db8eaeb5d898e9f0b75b093ea3c1
Anshul Sarathe <anshulsarathe35@gmail.com>
2023-11-22 16:13:26 +0530

    day3

---
+++ reverse_integer.java
@@ -0,0 +1,14 @@
+//Leetcode question 7
+
+class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        while(x!=0){
+            int rem = x%10;
+            x = x/10;
+            if(rev >0 && rev > (Integer.MAX_VALUE - rem)/ 10 || rev < 0 && rev < (Integer.MIN_VALUE - rem) /10) return 0;
+            rev = rev *10 + rem;
+        }
+        return rev;
+    }
+}
