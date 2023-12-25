1cb0e310b0a180d211ab0848a9fb7e4177742edb
Anshul Sarathe <anshulsarathe35@gmail.com>
2023-12-25 13:28:44 +0530

    day36

---
+++ assign_cookies.java
@@ -0,0 +1,19 @@
+//Leetcode question 455
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i=0;
+        int j=0;
+        int count = 0;
+        while(i<g.length && j<s.length){
+            if(s[j] >= g[i]){
+                count++;
+                i++;
+            }
+            j++;
+        }
+        return count;
+    }
+}
