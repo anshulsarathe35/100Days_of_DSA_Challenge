554c2f958c1b95d88967c94f1b62032e2d52fda1
Anshul Sarathe <anshulsarathe35@gmail.com>
2024-01-12 10:31:06 +0530

    day54

---
+++ determine_if_string_halves_alike.java
@@ -0,0 +1,30 @@
+//Leetcode question 1704
+
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        int n = s.length();
+        String str1 = s.substring(0,n/2);
+        String str2 = s.substring(n/2,n);
+        int vowelCount1 = 0;
+        int vowelCount2 = 0;
+        for(char ch: str1.toLowerCase().toCharArray()){
+            if(isVowel(ch)){
+                vowelCount1++;
+            }
+        }
+        for(char ch: str2.toLowerCase().toCharArray()){
+            if(isVowel(ch)){
+                vowelCount2++;
+            }
+        }
+        
+        if(vowelCount1==vowelCount2) return true;
+        return false;
+    }
+    static boolean isVowel(char ch){
+        if(ch == 'a' || ch=='e' || ch=='i' || ch=='o' ||ch=='u'){
+            return true;
+        }
+        return false;
+    }
+}
