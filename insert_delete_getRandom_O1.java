6a0488918fb89e7a4f10fbdcddf8dbea279191e4
Anshul Sarathe <anshulsarathe35@gmail.com>
2024-01-16 14:07:57 +0530

    day58

---
+++ insert_delete_getRandom_O1.java
@@ -0,0 +1,44 @@
+//Leetcode question 380
+
+class RandomizedSet {
+    ArrayList<Integer> list = new ArrayList<>();
+    HashMap<Integer, Integer> map= new HashMap<>();
+    Random random= new Random();
+    public RandomizedSet() {
+    }
+    
+    public boolean insert(int val) {
+        if (map.containsKey(val)) {
+            return false;
+        }
+        list.add(val);
+        map.put(val, list.size() - 1);
+        return true;
+    }
+    
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) {
+            return false;
+        }
+        int idx = map.get(val);
+        int lastElement = list.get(list.size() - 1);
+        list.set(idx, lastElement);
+        map.put(lastElement, idx);
+        list.remove(list.size() - 1);
+        map.remove(val);
+        return true;
+    }
+    
+    public int getRandom() {
+        int idx = random.nextInt(list.size());
+        return list.get(idx);
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
