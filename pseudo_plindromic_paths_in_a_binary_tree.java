3318a9745f83a49d8486eaf07c731002300e4257
Anshul Sarathe <anshulsarathe35@gmail.com>
2024-01-24 09:00:41 +0530

    day66

---
+++ pseudo_plindromic_paths_in_a_binary_tree.java
@@ -0,0 +1,46 @@
+//Leetcode question 1457
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pseudoPalindromicPaths(TreeNode root) {
+        int count = 0; 
+        int direction = 0;
+
+        Stack<Pair<TreeNode, Integer>> stack = new Stack<>();
+        stack.push(new Pair<>(root, 0));
+
+        while (!stack.isEmpty()) {
+            Pair<TreeNode, Integer> p = stack.pop();
+            TreeNode node = p.getKey();
+            direction = p.getValue();
+
+            if (node != null) {
+                direction = direction ^ (1 << node.val);
+
+                if (node.left == null && node.right == null) {
+                    if ((direction & (direction - 1)) == 0) {
+                        ++count;
+                    }
+                } else {
+                    stack.push(new Pair<>(node.right, direction));
+                    stack.push(new Pair<>(node.left, direction));
+                }
+            }
+        }
+        return count;
+    }
+}
