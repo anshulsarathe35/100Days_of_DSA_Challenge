e7b608f1f102dab645b80e298b01aabd54f54c84
Anshul Sarathe <anshulsarathe35@gmail.com>
2024-01-29 21:52:36 +0530

    day71

---
+++ implement_queue_using_stack.java
@@ -0,0 +1,34 @@
+//Leetcode question 232
+
+class MyQueue {
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void push(int x) {
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        stack1.push(x);
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+    }
+
+    public int pop() {
+        return stack1.pop();
+    }
+
+    public int peek() {
+        return stack1.peek();
+    }
+
+    public boolean empty() {
+        return stack1.isEmpty();
+    }
+}
+
